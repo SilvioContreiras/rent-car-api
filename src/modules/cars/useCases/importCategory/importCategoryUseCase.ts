57c25377ffa3a8505bc20147bf963630c1b1e96a
SilvioContreiras <silvio7dcontreiras@gmail.com>
2022-01-16 18:09:03 -0300

    fix type error

--- src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -13,13 +13,13 @@ class ImportCategoryUseCase {
     return new Promise((resolve, reject) => {
       const stream = fs.createReadStream(file.path);
       const categories: IImportRepository[] = [];
-
       const parseFile = parse();
 
       stream.pipe(parseFile);
-
+      
       parseFile
-        .on('data', async (line) => {
+      .on('data', async (line) => {
+          console.log(line)
           const [name, description] = line;
 
           categories.push({ name, description });
@@ -32,7 +32,7 @@ class ImportCategoryUseCase {
     });
   }
 
-  async execute(file: Express.Multer.File): Promise<void>   {
+  async execute(file: Express.Multer.File | any): Promise<void>    {
     const categories = await this.loadCategories(file);
 
     categories.map(async (category) => {
